2a32b227b9c4d9b68cfdea80e4b76b965e21f991
Dave Revell <revell@google.com>
2023-04-28 15:33:26 -0700

    First-phase server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/drevell/go-101
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "nobody"
+	}
+	fmt.Fprintf(w, "👋, %s!\n", name)
+}
+
+func main() {
+	http.HandleFunc("/hello", handler)
+
+	log.Print("Starting HTTP server...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
